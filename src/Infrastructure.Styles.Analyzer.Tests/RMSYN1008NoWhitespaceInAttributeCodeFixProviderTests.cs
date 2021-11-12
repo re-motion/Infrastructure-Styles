0a424c6d7fc6d58b6e41bb22285125ac65349696
Patrick Widauer <patrick.widauer@rubicon.eu>
2021-11-12 16:49:04 +0100

    RMTEAM-436 Implement RMSYN1008 No space in attribute

---
+++ src/Infrastructure.Styles.Analyzer.Tests/RMSYN1008NoWhitespaceInAttributeCodeFixProviderTests.cs
@@ -0,0 +1,99 @@
+// This file is part of the re-motion Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// re-motion Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+
+using Gu.Roslyn.Asserts;
+using NUnit.Framework;
+
+[assembly: MetadataReference(typeof(object))]
+
+namespace Infrastructure.Styles.Analyzer.Tests
+{
+  public class RMSYN1008NoWhitespaceInAttributeCodeFixProviderTests
+  {
+    private static readonly RMSYN1008NoWhitespaceInAttributeAnalyzer Analyzer = new();
+    private static readonly RMSYN1008NoWhitespaceInAttributeCodeFixProvider CodeFixProvider = new();
+
+    [Test]
+    public void CodeFix_WithNoWhitespace()
+    {
+      var before = @"
+namespace N
+{
+    [System.Serializable↓ ()]
+    class C
+    {
+    }
+}";
+
+      var after = @"
+namespace N
+{
+    [System.Serializable()]
+    class C
+    {
+    }
+}";
+      RoslynAssert.CodeFix(Analyzer, CodeFixProvider, before, after);
+    }
+
+    [Test]
+    public void CodeFix_WithTwoWhitespaces()
+    {
+      var before = @"
+namespace N
+{
+    [System.Serializable↓   ()]
+    class C
+    {
+    }
+}";
+
+      var after = @"
+namespace N
+{
+    [System.Serializable()]
+    class C
+    {
+    }
+}";
+      RoslynAssert.CodeFix(Analyzer, CodeFixProvider, before, after);
+    }
+
+    [Test]
+    public void CodeFix_WithNewLine()
+    {
+      var before = @"
+namespace N
+{
+    [System.Serializable↓
+         ()]
+    class C
+    {
+    }
+}";
+
+      var after = @"
+namespace N
+{
+    [System.Serializable()]
+    class C
+    {
+    }
+}";
+      RoslynAssert.CodeFix(Analyzer, CodeFixProvider, before, after);
+    }
+  }
+}
---
+++ src/Infrastructure.Styles.Analyzer/RMSYN1008NoWhitespaceInAttributeAnalyzer.cs
@@ -0,0 +1,72 @@
+// This file is part of the re-motion Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// re-motion Framework is free software; you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Infrastructure.Styles.Analyzer
+{
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class RMSYN1008NoWhitespaceInAttributeAnalyzer : DiagnosticAnalyzer
+  {
+    public const string DiagnosticId = "RMSYN1008";
+
+    internal const string Title = "Invalid number of whitespaces in attribute between the identifier and the argument list";
+    internal const string Message = "There must be no whitespace in an attribute between the identifier and the argument list";
+    internal const string Description = "Attribute must not contain a space between the identifier and the argument list";
+    internal const string Category = "Formatting";
+
+    private static readonly DiagnosticDescriptor Rule = new(
+        DiagnosticId,
+        Title,
+        Message,
+        Category,
+        DiagnosticSeverity.Warning,
+        true,
+        Description);
+
+    public override void Initialize (AnalysisContext context)
+    {
+      context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+      context.EnableConcurrentExecution();
+
+      context.RegisterSyntaxNodeAction(AnalyzeAttribute, SyntaxKind.Attribute);
+    }
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+    private static void AnalyzeAttribute (SyntaxNodeAnalysisContext context)
+    {
+      var attributeSyntax = (AttributeSyntax) context.Node;
+      var left = attributeSyntax.Name.GetLastToken();
+      var right = attributeSyntax.ArgumentList?.OpenParenToken;
+      
+      if (!right.HasValue)
+        return;
+
+      var newRight = right.Value;
+      SimpleWhitespaceAnalyzer.AnalyzeNoWhitespaceBetweenTokens(
+          ref context,
+          Rule,
+          in left,
+          in newRight);
+    }
+  }
+}
