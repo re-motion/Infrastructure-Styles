335bd452ceacbfb02a70306064a3307ccc96eef1
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2022-06-13 12:26:15 +0200

    Added first tests for codefixer and appropriate solutions

--- src/Infrastructure.Styles.Analyzer.Tests/RMSYN1102SeparateExitConditionsCodeFixProviderTests.cs
+++ src/Infrastructure.Styles.Analyzer.Tests/RMSYN1102SeparateExitConditionsCodeFixProviderTests.cs
@@ -14,10 +14,85 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 
+using Gu.Roslyn.Asserts;
+using NUnit.Framework;
+
 namespace Infrastructure.Styles.Analyzer.Tests
 {
+  [TestFixture]
   public class RMSYN1102SeparateExitConditionsCodeFixProviderTests
   {
+    private static readonly RMSYN1102SeparateExitConditionsAnalyzer Analyzer = new();
+    private static readonly RMSYN1102SeparateExitConditionsCodeFixProvider CodeFixProvider = new();
+
+    [Test]
+    public void CodeFix_SimpleConditionWithReturn_CreatesProperIFStatements ()
+    {
+      var before = @"
+namespace N
+{
+    class C
+    {
+
+        private void Main()
+        {
+            if(↓1 > 2 || 2 > 1)
+                return;
+        }
+    }
+}";
+      var after = @"
+namespace N
+{
+    class C
+    {
+
+        private void Main()
+        {
+            if(1 > 2)
+                return;
+            if(2 > 1)
+                return;
+        }
+    }
+}";
+      RoslynAssert.CodeFix(Analyzer, CodeFixProvider, before, after);
+    }
     
+    [Test]
+    public void CodeFix_ComplexConditionWithReturnStatement_CreatesProperIFStatements ()
+    {
+        var before = @"
+namespace N
+{
+    class C
+    {
+
+        private void Main()
+        {
+            if(↓(1 > 2 && 3 > 4) || (2 > 1 || 4 > 5))
+                return;
+        }
+    }
+}";
+        var after = @"
+namespace N
+{
+    class C
+    {
+
+        private void Main()
+        {
+            if((1 > 2 && 3 > 4))
+                return;
+            if(2 > 1)
+                return;
+            if(4 > 5)
+                return;
+        }
+    }
+}";
+        RoslynAssert.CodeFix(Analyzer, CodeFixProvider, before, after);
+    }
   }
 }
--- src/Infrastructure.Styles.Analyzer/RMSYN1102SeparateExitConditionsCodeFixProvider.cs
+++ src/Infrastructure.Styles.Analyzer/RMSYN1102SeparateExitConditionsCodeFixProvider.cs
@@ -15,6 +15,7 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Linq.Expressions;
@@ -23,24 +24,26 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Infrastructure.Styles.Analyzer
 {
   public class RMSYN1102SeparateExitConditionsCodeFixProvider : CodeFixProvider
   {
-    internal struct DoubleExpressions
+    internal struct SplitExpressions
     {
       public ExpressionSyntax left { get; private set; }
       public ExpressionSyntax right { get; private set; }
 
-      public DoubleExpressions (ExpressionSyntax left, ExpressionSyntax right)
+      public SplitExpressions (ExpressionSyntax left, ExpressionSyntax right)
       {
         this.left = left;
         this.right = right;
       }
     }
-    
+
     public override FixAllProvider? GetFixAllProvider ()
     {
       return WellKnownFixAllProviders.BatchFixer;
@@ -68,40 +71,98 @@ namespace Infrastructure.Styles.Analyzer
       var syntaxRoot = await context.Document.GetSyntaxRootAsync(cancellationToken);
       if (syntaxRoot == null) return context.Document;
 
-      DoubleExpressions orExpressions;
-      IfStatementSyntax? ifStatement;
-      var orPattern = syntaxRoot.FindNode(diagnosticSpan).FirstAncestorOrSelf<PatternSyntax>();
-      var logicalOrExpression = syntaxRoot.FindNode(diagnosticSpan).FirstAncestorOrSelf<BinaryExpressionSyntax>();
-      if (orPattern != null)
-      {
-        orExpressions = SplitOrPattern(orPattern);
-        ifStatement = orPattern.Parent!.Parent as IfStatementSyntax;
-      }
-      else if (logicalOrExpression != null)
-      {
-        orExpressions = SplitLogicalOrExpression(logicalOrExpression);
-        ifStatement = logicalOrExpression.Parent as IfStatementSyntax;
-      }
-      else
-      {
-        return context.Document;
-      }
+      return context.Document.WithSyntaxRoot(ReplaceIfStatement(syntaxRoot, diagnosticSpan));
+    }
+
+    private static SyntaxNode ReplaceIfStatement (SyntaxNode syntaxRoot, TextSpan diagnosticSpan)
+    {
+      var illegalExpression = syntaxRoot.FindNode(diagnosticSpan);
+      
+
+      RMSYN1102SeparateExitConditionsAnalyzer.IsImmediateParentOrParentsParentIfStatement(illegalExpression,
+        out var ifStatement);
 
       if (ifStatement == null)
-        return context.Document;
-      var ifChildStatement = ifStatement.Statement;
-      
-      return context.Document;
+        return syntaxRoot;
+
+      var syntaxNodes = new List<SyntaxNode>();
+      FillSyntaxNodeListWithProperIfStatementsFromOrExpression(syntaxNodes, ifStatement, (ExpressionSyntax) illegalExpression);
+
+      var newRoot = syntaxRoot.ReplaceNode(ifStatement, syntaxNodes);
+
+      return newRoot;
     }
 
-    private static DoubleExpressions SplitLogicalOrExpression (BinaryExpressionSyntax orNode)
+    private static SplitExpressions SplitOrSyntax (ExpressionSyntax expression)
     {
-      return new DoubleExpressions(orNode.Left, orNode.Right);
+      if (expression.IsKind(SyntaxKind.LogicalOrExpression))
+      {
+        var orNode = (expression as BinaryExpressionSyntax)!;
+        return new SplitExpressions(orNode.Left, orNode.Right);
+      }
+
+      if (expression.IsKind(SyntaxKind.OrPattern))
+      {
+        throw new NotImplementedException();
+      }
+
+      throw new InvalidOperationException($"Could not split expression '{expression}'");
     }
 
-    private static DoubleExpressions SplitOrPattern (PatternSyntax orPattern)
+
+    private static bool IsIllegalExpression (SyntaxNode expression, out ExpressionSyntax? illegalExpression)
     {
-      throw new NotImplementedException();
+      if (expression.IsKind(SyntaxKind.ParenthesizedExpression))
+      {
+        var parenthesizedExpression = expression as ParenthesizedExpressionSyntax;
+        return IsIllegalExpression(parenthesizedExpression!.Expression, out illegalExpression);
+      }
+      if (expression.IsKind(SyntaxKind.LogicalOrExpression) || expression.IsKind(SyntaxKind.OrPattern))
+      {
+        illegalExpression = expression as ExpressionSyntax;
+        return true;
+      }
+
+      illegalExpression = null;
+      return false;
+    }
+
+    private static void FillSyntaxNodeListWithProperIfStatementsFromOrExpression (List<SyntaxNode> syntaxNodes, IfStatementSyntax ifBlueprint, ExpressionSyntax expression)
+    {
+      while (true)
+      {
+        if (IsIllegalExpression(expression, out var illegalExpression))
+        {
+          var splitExpression = SplitOrSyntax(illegalExpression!);
+          splitExpression = RemoveRedundantTrivia(splitExpression);
+          FillSyntaxNodeListWithProperIfStatementsFromOrExpression(syntaxNodes, ifBlueprint, splitExpression.left);
+          expression = splitExpression.right;
+          continue;
+        }
+
+        var nextIfStatement = ifBlueprint.WithCondition(expression);
+        syntaxNodes.Add(nextIfStatement);
+        break;
+      }
+    }
+
+    private static SplitExpressions RemoveRedundantTrivia (SplitExpressions splitExpression)
+    {
+      return  new SplitExpressions(RemoveTrailingTrivia(splitExpression.left),
+        RemoveLeadingTrivia(splitExpression.right));
+    }
+
+    private static ExpressionSyntax RemoveLeadingTrivia (ExpressionSyntax expression)
+    {
+      var tokenWithRemoved = expression.FindToken(expression.SpanStart).WithLeadingTrivia(SyntaxTriviaList.Empty);
+      return expression.ReplaceToken(expression.FindToken(expression.SpanStart), tokenWithRemoved);
+    }
+
+    private static ExpressionSyntax RemoveTrailingTrivia (ExpressionSyntax expression)
+    {
+      var tokenWithRemoved = expression.FindToken(expression.Span.End).WithTrailingTrivia(SyntaxTriviaList.Empty);
+      return expression.ReplaceToken(expression.FindToken(expression.Span.End), tokenWithRemoved);
     }
   }
+  
 }
